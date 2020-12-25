680d42b1b35356f14a8583db85ce879d8cc7bc92
vblll <vb.mbox@gmail.com>
2020-12-25 13:52:25 +0300

    start Cot data API csv parser

--- Cotr.CotAPI/CotDataRepository.cs
+++ Cotr.CotAPI/CotDataRepository.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Net;
+using System.IO;
+using System.IO.Compression;
+using CsvHelper;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace Cotr.CotAPI
 {
@@ -7,9 +12,11 @@ namespace Cotr.CotAPI
     {
         private static readonly string commodityArchiveUrl = @"https://www.cftc.gov/files/dea/history/";
         private static readonly string commodityArcFileName = "fut_disagg_txt_2020.zip";
+        private static readonly string commodityArcFileCsv = "f_year.txt";
 
         private static readonly string financialArchiveUrl = @"https://www.cftc.gov/files/dea/history/";
         private static readonly string financialArcFileName = "fut_fin_txt_2020.zip";
+        private static readonly string financialArcFileCsv = "FinFutYY.txt";
 
         private static readonly string tmpDir = @"D:\tmp\";
 
@@ -20,14 +27,32 @@ namespace Cotr.CotAPI
 
         private void Download(string url, string fileName)
         {
+            string path = tmpDir + fileName;
+
             WebClient client = new WebClient();
-            client.DownloadFile(url + fileName, tmpDir + fileName);
+            client.DownloadFile(url + fileName, path);
+            ZipFile.ExtractToDirectory(path, tmpDir);
         }
 
         public void GetCotArchiveData()
         {
+            IEnumerable<dynamic> commodityRecords;
+            IEnumerable<dynamic> financialRecords;
+
             this.Download(commodityArchiveUrl, commodityArcFileName);
             this.Download(financialArchiveUrl, financialArcFileName);
+
+            using (var reader = new StreamReader(tmpDir + commodityArcFileCsv))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                commodityRecords = csv.GetRecords<dynamic>();
+            }
+
+            using (var reader = new StreamReader(tmpDir + financialArcFileCsv))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                financialRecords = csv.GetRecords<dynamic>();
+            }
         }
     }
 }
--- ServiceManager/CotDataService.cs
+++ ServiceManager/CotDataService.cs
@@ -20,7 +20,7 @@ namespace Cotr.Service
             SM = SM ?? sm;
         }
 
-        private void updateFromArchive()
+        private void UpdateFromArchive()
         {
             CotRepo.GetCotArchiveData();
         }
@@ -33,7 +33,7 @@ namespace Cotr.Service
             // if last date is empty (01.01.0001 0:00:00),
             // get reports archive from API and save to DB
             if (dbDataLastDate < Convert.ToDateTime("01/01/1970"))
-                this.updateFromArchive();
+                this.UpdateFromArchive();
 
             // upload report from API and get last date
             // if report date not equal to last date from DB, save new report data to DB
